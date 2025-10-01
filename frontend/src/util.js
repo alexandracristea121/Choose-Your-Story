09e1e620311abc93cf3c4a747a350301277394fd
Alexandra Cristea <alexandracristea121@gmail.com>
2025-10-01 19:20:26 +0300

    Try using window configs

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,4 +1,3 @@
-//export const API_BASE_URL = "http://localhost:8000";
-//export const API_BASE_URL = "/api"
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+  (window.configs && window.configs.API_BASE_URL) ||
+  "http://localhost:8000/api";
